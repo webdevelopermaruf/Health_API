f6dd09fbb0052188f61bbce4ba43e83c4a725b26
Md Maruf <web.developer.mdmaruf@gmail.com>
2025-08-22 06:42:26 +0100

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/get-report/{invoice}/{mobile_number}', [\App\Http\Controllers\BillingController::class, 'report']);
+Route::get('/storage', function () {
+    Artisan::call('storage:link');
+});
